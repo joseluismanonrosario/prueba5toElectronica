c4cceae9063163677ddf9c3087cfc61da26a9c61
Jose Luis Manon <joseluismanonrosario@gmail.com>
2026-04-23 13:20:22 -0400

    Agrega contraseña a la app

--- app.c
+++ app.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     int a = 5;
     int b = 10; 
     int sum = a + b;
-    printf("The sum of %d and %d is %d\n", a, b, sum);
+
+
+    printf("Ingrese su contraseña: ");
+    char password[20];
+    scanf("%s", password);
+    if (strcmp(password, "123456") == 0) {
+        printf("The sum of %d and %d is %d\n", a, b, sum);
+        printf("Creado por: ChatGPT\n");
+    } else {
+        printf("Contraseña incorrecta. Acceso denegado.\n");
+    }
+    
     return 0;
 }
